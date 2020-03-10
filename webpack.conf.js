7e550c749b6b04a179381b2ea3bf17ee28bcbee3
Thomas Cholley <cholley.t@gmail.com>
2020-03-10 10:17:24 +0100

    Build & bundle processes to output bundled library, index.d.ts and modules library.

---
+++ webpack.conf.js
@@ -0,0 +1,48 @@
+const { resolve } = require("path");
+const webpack = require("webpack");
+const { name, version } = require(resolve(__dirname, "package.json"));
+const [, baseName] = name.split("/");
+
+module.exports = {
+  entry: resolve(__dirname, "dist/build/index.js"),
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: `${baseName}.min.js`,
+    library: name,
+    libraryTarget: "umd",
+  },
+  externals: [
+    "opentype.js"
+  ],
+  module: {
+    rules: [{
+      test: /.js/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                useBuiltIns: "entry",
+                corejs: 3,
+                targets: {
+                  "ie": "11"
+                }
+              }
+            ]
+          ],
+        },
+      },
+    }],
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `${name} ${version} ${new Date().toString()}`,
+    }),
+  ]
+};
